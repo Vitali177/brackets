426835e734fcc316873a63e9dd53fce5aba857a4
Vitali <vskrabatun@gmail.com>
2019-10-02 18:16:06 +0300

    feat: add second version of brackets task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  var firstOpenBrackets = 0;
-  var firstCloseBrackets = 0;
-  var prov = 0;
+  
+
 
     
+    //str = '5555512575557777777555566667888888667661133833448441111222233333444442266666';
+    //bracketsConfig = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
 
     var flagSameBracketOpened = false;
-    flagSameBracketOpened = false;  
 
     // надо написать функцию для проверки, чтобы узнать скобка является открытой или закрытой
 
@@ -17,67 +17,105 @@ module.exports = function check(str, bracketsConfig) {
             if (bracketsConfig[key][0] === bracket)
                 return true;   // вернет тру, если скобка открытая, или открытая скобка и закрытая -  одинаковы         
         }
-        return false;
+        return false;   // Если скобка закрывающая
     }
 
     var arr = [];
 
     for (var i = 0; i < str.length; i++) {
 
-        for (let key in bracketsConfig) {   
+        flagSameBracketOpened = false;
 
-            if (bracketsConfig[key][0] === str[i]) {
+        for (let key in bracketsConfig) {               
 
+            if (bracketsConfig[key][0] === str[i]) {   // открытую скобку закидываем в стек
+
+                //console.log( i + '   '  + str[i]);
+                
                 arr.push(str[i]);
-                break;   
 
-                /*if ( bracketsConfig[key][0] === bracketsConfig[key][1]  ) { // если открытая и закрытая скобка одинакавы
+                if ( bracketsConfig[key][0] === bracketsConfig[key][1]  ) { // если открытая и закрытая скобка одинакавы
 
-                    // если следующей скобки нет, а предыдущая такая же
-                    console.log('str: ' + str + 'str[i]  ' + str[i] + 'str[i+1]  ' + str[i+1] + 'i =  ' + i);
-                    if (str[i+1] === undefined) {
-                        if (str[i] === str[i-1]) {
-                            console.log('hihi');
-                            flagSameBracketOpened = true;
-                        }
+                    // если скобка первая в строке, то она открывающаяся
+
+                    //console.log( 'same   '   +  i + '   '  + str[i]);
+
+                    if ( i === 0) { 
+                        //console.log('first');
+                        flagSameBracketOpened = true;
+                        break;
                     }
 
-                    // надо узнать относится ли наша скобка к открытой либо закрытой
+                    // если скобка последняя в строке, то она закрывающаяся
 
-                    // если предыдущая скобка открыта и следующая закрытая, то мы не устанавливаем тру флаг потому что потом 
-                    //мы закинем эту скобку в массив и сразу же достанем в следующем цикле, что в принципе правильно по условию задания
-                    if (checkBracketIsOpened(str[i-1], bracketsConfig) && !checkBracketIsOpened(str[i+1], bracketsConfig) ) {
-                        flagSameBracketOpened = true;  
-                    } 
-                    // если предыдущей скобки нет и следующая закрытая или такая  же
-                    if (str[i-1] === undefined) {
-                        console.log(str[i] + '  ' + str);
-                        if  ( !checkBracketIsOpened(str[i+1], bracketsConfig)) {
-                            flagSameBracketOpened = true;
-                        } else if (str[i] === str[i+1]) { 
-                            console.log('hi');
-                            flagSameBracketOpened = true;
-                        }                
-                    }                    
-                    if (checkBracketIsOpened(str[i-1], bracketsConfig) ) {
-                        console.log('');
-                    }              
+                    if (i ===  (str.length - 1) ) {
+                        //console.log('there');
+                        arr.pop();
+                        flagSameBracketOpened = false;
+                        break;
+                    }
+
+
+                    // если предыдущая такая же
+
+                    if (str[i] === str[i-1]) {
+                        // последний элемент и есть наша str[i], так как мы выше его запушили, надо проверить на предпоследний элемент, если это скобка такая же, то мы их две сократим, достав с массива две этих скобки
+    
+                            if (arr[arr.length - 2] === str[i]) {
+                                //console.log('pop 2');
+                                arr.pop();
+                                arr.pop();
+                                flagSameBracketOpened = true;   // ставим тру, так как мы их достали
+                                break;
+                            }   
+                        }
+
+
+                    //  следующая такая же, а предыдущая открывающаяся, то она открывающаяся
+
+                    if (str[i+1] === str[i] && checkBracketIsOpened(str[i-1], bracketsConfig) ) {
+                        flagSameBracketOpened = true;
+                        break;
+                    }
+
+
                     
-                    // Если предыдущая скобка открытая и следующая закрытая          
-                }*/  
+
+
+
+                    // предыдущая такая же, то она закрывающаяся
+
+                    /*if (str[i] === str[i-1]) {
+                        arr.pop();
+                        flagSameBracketOpened = false;
+                        break;
+                    }*/
+
+                    
+                    
+                    
+                                     
+                             
+                } 
+                break;
             }                          
             
         }
+        //console.log(arr);
+        //console.log('flagSameBracketOpened   ' + flagSameBracketOpened);
 
-        //if (flagSameBracketOpened) break; // если наша скобка относится к открытой
+        if (flagSameBracketOpened) continue; // если наша скобка относится к открытой
 
         for (let key in bracketsConfig) {
 
             if (bracketsConfig[key][1] === str[i]) {
-                console.log(arr);                
+                //console.log(arr);                
             
-                if (arr[arr.length - 1] !== bracketsConfig[key][0]) 
+                if (arr[arr.length - 1] !== bracketsConfig[key][0])  {
+                    //console.log('false');
                     return false;
+                }
+                    
                 else {
                     arr.pop();
                     break;
